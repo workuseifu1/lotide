f05addaaef5566a4edd5cdb6a1cd253303196af8
workuseifu1 <workuseifu1@gmail.com>
2023-04-20 19:00:53 -0500

    Implemented head

---
+++ head.js
@@ -0,0 +1,16 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const head = function(array) {
+  let firstArray = "";
+  for(let i = 0 ; i < array.length ; i++){
+    firstArray = array[0];
+  }
+  return firstArray;
+};
+assertEqual(head([]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");

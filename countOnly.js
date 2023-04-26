3c35c50fc1d68184391895890162789e75f1bf5f
workuseifu1 <workuseifu1@gmail.com>
2023-04-26 15:48:11 -0500

    Added countOnly.js

---
+++ countOnly.js
@@ -0,0 +1,38 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const countOnly = function(allItems, itemsToCount) {
+  const results = {};
+  for (const name of allItems) {
+    if (itemsToCount[name]) {
+      if (results[name]) {
+        results[name] += 1;
+      } else {
+        results[name] = 1;
+      }
+    }
+  }
+  return results;
+};
+const firstNames = [
+  "Karl",
+  "Salima",
+  "Agouhanna",
+  "Fang",
+  "Kavith",
+  "Jason",
+  "Salima",
+  "Fang",
+  "Joe"
+];
+
+const result1 = countOnly(firstNames, { "Jason": true, "Karima": true, "Fang": true, "Agouhanna": false });
+
+assertEqual(result1["Jason"], 1);
+assertEqual(result1["Karima"], undefined);
+assertEqual(result1["Fang"], 2);
+assertEqual(result1["Agouhanna"], undefined);

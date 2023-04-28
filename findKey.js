02f1a530b437c0397c6e0dd27e3c0b398a426943
workuseifu1 <workuseifu1@gmail.com>
2023-04-28 02:21:24 -0500

    Added findKey.js

---
+++ findKey.js
@@ -0,0 +1,28 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+const findKey = function(object,callback) {
+  let results = "";
+  for (const key in object) {
+    let keys = object[key];
+    if (callback(keys)) {
+      results += key;
+      break;
+    }
+  }
+  return results;
+};
+const result1 = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2);
+console.log(result1);
+assertEqual(result1, "noma");

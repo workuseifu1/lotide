57b038c069f8e5730f59bf255fccc92c215772a1
workuseifu1 <workuseifu1@gmail.com>
2023-05-10 20:00:24 -0500

    Completed

--- eqArrays.js
+++ eqArrays.js
@@ -1,24 +1,13 @@
-const assertEqual = function (actual, expected) {
-  if (actual === expected) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-const eqArrays = function (arr1, arr2) {
+const eqArrays = function(arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
-  for (let i = 0; i < arr1.length; i++) {
+  for (let i in arr1) {
     if (arr1[i] !== arr2[i]) {
       return false;
     }
   }
   return true;
 };
+module.exports = eqArrays;
 
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
-assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
-
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
-assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false

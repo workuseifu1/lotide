2fcb6fd42e6eb436102f9e5adc18dffce49662ed
workuseifu1 <workuseifu1@gmail.com>
2023-05-10 22:38:47 -0500

    completed

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,8 +1,15 @@
-const middle =require('../middle');
-const assertArraysEqual = require('../assertArraysEqual');
 
-assertArraysEqual(middle([1, 2, 3, 4, 5,6]), [3, 4]);
-assertArraysEqual(middle([1]), []);
-assertArraysEqual(middle([1, 2]), []);
-assertArraysEqual(middle([1, 2, 3]),[2]);
-assertArraysEqual(middle([1, 2, 3, 4, 5]),[3]);
+const assert = require('chai').assert;
+const middle = require('../middle');
+
+describe("#middle", () => {
+  it("returns [3, 4] for [1, 2, 3, 4, 5,6]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5, 6]), [3, 4]);
+  });
+  it("returns [3] for [1, 2, 3, 4, 5]", () => {
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
+  });
+  it("returns [] for [1, 2]", () => {
+    assert.deepEqual(middle([1, 2]), []);
+  });
+});

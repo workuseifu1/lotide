e6d1321e1480dd395c5a514d158fa16d0a58b62c
workuseifu1 <workuseifu1@gmail.com>
2023-05-10 19:39:50 -0500

    Completed

--- head.js
+++ head.js
@@ -1,16 +1,12 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-const head = function(array) {
+const head = function (array) {
   let firstArray = "";
-  for (let i = 0 ; i < array.length ; i++) {
+  if (array.length === 0) {
+    return "undefined";
+  } else {
+  for (let i = 0; i < array.length; i++) {
     firstArray = array[0];
   }
   return firstArray;
+}
 };
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+module.exports = head;

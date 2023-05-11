22c0f693af29b777df3aa6e1505a6d7b0de2d432
workuseifu1 <workuseifu1@gmail.com>
2023-05-10 19:53:40 -0500

    completed

--- tail.js
+++ tail.js
@@ -1,20 +1,5 @@
-const assertEqual = function(actual, expected) {
-  
-  if (actual.toString() === expected.toString()) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-
-};
-const tail = function(array) {
+const tail = function (array) {
   let result = array.slice(1);
   return result;
 };
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
-const results = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(results.length, 2);
-assertEqual(results[0], "Lighthouse");
-assertEqual(results[1], "Labs");
+module.exports = tail;

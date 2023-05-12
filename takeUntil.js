316a2aee1b6ce5b47fdf5887af13986d52d88382
workuseifu1 <workuseifu1@gmail.com>
2023-05-12 12:04:37 -0500

    Completed

--- takeUntil.js
+++ takeUntil.js
@@ -1,4 +1,4 @@
-const eqArrays = function(arr1, arr2) {
+const eqArrays = function (arr1, arr2) {
   if (arr1.length !== arr2.length) {
     return false;
   }
@@ -10,14 +10,14 @@ const eqArrays = function(arr1, arr2) {
   return true;
 };
 
-const assertArraysEqual = function(arr1,arr2) {
-  if (eqArrays(arr1,arr2)) {
+const assertArraysEqual = function (arr1, arr2) {
+  if (eqArrays(arr1, arr2)) {
     console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
   } else {
     console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
   }
 };
-const takeUntil = function(array, callback) {
+const takeUntil = function (array, callback) {
   const results = [];
   for (const item of array) {
     if (!callback(item)) {
@@ -25,17 +25,7 @@ const takeUntil = function(array, callback) {
     } else {
       break;
     }
-    
   }
   return results;
 };
-const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
-const results1 = takeUntil(data1, x => x < 0);
-console.log(results1);
-console.log('---');
-
-const data2 = ["I've", "been", "to", "Hollywood", ",", "I've", "been", "to", "Redwood"];
-const results2 = takeUntil(data2, x => x === ',');
-console.log(results2);
-assertArraysEqual(results1, [ 1, 2, 5, 7, 2 ]);
-assertArraysEqual(results2, [ 'I\'ve', 'been', 'to', 'Hollywood' ]);
+module.exports = takeUntil;

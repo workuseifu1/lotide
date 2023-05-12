911cd18d21c7e56d03f31327e853af0d2cb2ac2d
workuseifu1 <workuseifu1@gmail.com>
2023-05-12 18:29:29 -0500

    complet

---
+++ test/assertObjectsEqualTest.js
@@ -0,0 +1,3 @@
+const assertObjectsEqual = require('../assertObjectsEqual');
+assertObjectsEqual([1, 2, 3], [1, 2, 3]);
+assertObjectsEqual({ a: '1', b: 2 }, { a: '1', b: 2 });
---
+++ test/eqObjectsTest.js
@@ -0,0 +1,9 @@
+const eqObjects =require('../eqObjects');
+const assertEqual =require('../assertEqual');
+
+const shirtObject = { color: "red", size: "medium" };
+const anotherShirtObject = { size: "medium", color: "red" };
+assertEqual(eqObjects(shirtObject, anotherShirtObject), true); 
+
+const longSleeveShirtObject = { size: "medium", color: "red", sleeveLength: "long" };
+assertEqual(eqObjects(shirtObject, longSleeveShirtObject), false); 

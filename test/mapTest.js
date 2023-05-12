e9a8b1a168cb4008c4452d4132c5310b7dfd4ec4
workuseifu1 <workuseifu1@gmail.com>
2023-05-12 16:56:14 -0500

    update discription

--- test/mapTest.js
+++ test/mapTest.js
@@ -2,7 +2,7 @@ const assert = require('chai').assert;
 const index = require('../index');
 
 describe("#map", () => {
-  it("returns [ 'g', 'c', 't', 'm', 't' ] for ['ground', 'control', 'to', 'major', 'tom']", () => {
+  it("returns [ 'g', 'c', 't', 'm', 't' ] for an array of['ground', 'control', 'to', 'major', 'tom'] and the condition of callback function is first letter of each elemets ", () => {
     assert.deepEqual(index.map(["ground", "control", "to", "major", "tom"], word => word[0]), [ 'g', 'c', 't', 'm', 't' ]);
   });  
 });

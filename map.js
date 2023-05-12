16b7944dd98b55be2a94c1b43425011119a8ee82
workuseifu1 <workuseifu1@gmail.com>
2023-05-12 12:15:52 -0500

    Completed

--- map.js
+++ map.js
@@ -1,22 +1,3 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-
-const assertArraysEqual = function(arr1,arr2) {
-  if (eqArrays(arr1,arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} !== ${arr2}`);
-  }
-};
 const map = function(array,callback) {
   const results = [];
   for (let item of array) {
@@ -24,6 +5,4 @@ const map = function(array,callback) {
   }
   return results;
 };
-const words = ["ground", "control", "to", "major", "tom"];
-const result1 = map(words, word => word[0]);
-assertArraysEqual(result1, [ 'g', 'c', 't', 'm', 't' ]);
+module.exports = map;

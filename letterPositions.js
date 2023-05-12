8a8bb267ca805208210c778d0f20a49284c9ef82
workuseifu1 <workuseifu1@gmail.com>
2023-05-12 12:31:20 -0500

    Completed

--- letterPositions.js
+++ letterPositions.js
@@ -1,22 +1,4 @@
-const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length) {
-    return false;
-  }
-  for (let i = 0; i < arr1.length; i++) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
-    }
-  }
-  return true;
-};
-const assertArraysEqual = function(arr1,arr2) {
-  if (eqArrays(arr1,arr2)) {
-    console.log(`✅✅✅ Assertion Passed: ${arr1} === ${arr2}`);
-  } else {
-    console.log(`🛑🛑🛑 Assertion Failed: ${arr1} != ${arr2}`);
-  }
-};
-const letterPositions = function(sentence) {
+const letterPositions = function (sentence) {
   const results = {};
   for (const position in sentence) {
     const letter = sentence[position];
@@ -30,6 +12,5 @@ const letterPositions = function(sentence) {
   }
   return results;
 };
-const word = "lighthouse in the house";
-console.log(letterPositions(word));
-assertArraysEqual(letterPositions("hello").e, [1]);
+module.exports = letterPositions;
+

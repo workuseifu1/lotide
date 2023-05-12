7de31a69bd8dcf044cdd3cb2dca5e02f9e29dd35
workuseifu1 <workuseifu1@gmail.com>
2023-05-12 16:48:54 -0500

    update the discription

--- test/withoutTest.js
+++ test/withoutTest.js
@@ -2,10 +2,10 @@ const assert = require('chai').assert;
 const index = require('../index');
 
 describe("#without", () => {
-  it('returns ["1", "2"] for ["1", "2", "3"]', () => {
+  it('returns ["1", "2"] for array of ["1", "2", "3"] and unwanted element of [1, 2, "3"] ', () => {
     assert.deepEqual(index.without(["1", "2", "3"],[1, 2, "3"]),["1", "2"]);
   });
-  it("returns [2, 3] for ([1, 2, 3], [1])", () => {
+  it("returns [2, 3] for array of [1, 2, 3] and unwantyed element of [1])", () => {
     assert.deepEqual(index.without([1, 2, 3], [1]), [2, 3]);
   });  
 });

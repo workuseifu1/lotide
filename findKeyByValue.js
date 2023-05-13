6f5bc1f1a209eea0955012d0fbbaf815e6cb9545
workuseifu1 <workuseifu1@gmail.com>
2023-05-12 19:09:37 -0500

    Complete

--- findKeyByValue.js
+++ findKeyByValue.js
@@ -1,26 +1,14 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    return console.log(`✅✅✅ Assertion Passed: ${actual} === ${expected}`);
-  } else {
-    return console.log(`🛑🛑🛑 Assertion Failed: ${actual} !== ${expected}`);
-  }
-};
-const findKeyByValue = function(obj, value) {
+const findKeyByValue = function (obj, value) {
   let keys = "";
   for (const key in obj) {
-    const letter = obj[key];
-    if (letter === value) {
-      keys += key;
+    if (obj[key] === value) {
+      keys = key;
+      break;
+    } else if (obj[key] !== value) {
+      keys = undefined;
+      break;
     }
   }
   return keys;
 };
-const bestTVShowsByGenre = {
-  sciFi: "The Expanse",
-  comedy: "Brooklyn Nine-Nine",
-  drama:  "The Wire"
-};
-
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
-assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
-assertEqual(findKeyByValue(bestTVShowsByGenre, "The Expanse"), "sciFi");
+module.exports = findKeyByValue;
